c1101d3a06116a69725c38567187df323a3896a0
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-08-30 17:02:26 -0700

    working on heroku

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 'use strict';
 
+const pg = require('pg');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const conString = process.env.DATABASE_URL || 'postgres://localhost:5432'; // DONE: Don't forget to set your own conString
+const client = new pg.Client(conString);
+
 app.use(express.static('./public'));
+
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
 
 app.listen(PORT);
